3988a3b8df795b7232c216fcb814a4b2d614c91e
runnerbos <runnerbos@users.noreply.github.com>
2015-02-17 16:28:56 -0700

    Added some js files.

---
+++ CertainDeath/Content/Scripts/game.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="phaser.js" />
+
+var game = new Phaser.Game("", "", Phaser.AUTO, "body", { preload: preload, create: create, update: update, render: render });
+
+var preload = new function () {
+    // download all sprites
+}
+
+var create = new function () {
+    // add all objects to game to be displayed
+}
+
+var update = new function () {
+    // called every frame
+}
+
+var render = new function () {
+    // 
+}
