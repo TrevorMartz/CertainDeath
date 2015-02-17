9370aca9b8d4a39dc4767141d91707c2b47e43ff
Blake Rollins <brollins90@gmail.com>
2015-02-17 16:13:44 -0700

    added basic DAL

---
+++ CertainDeathEngine/DAL/IDAL.cs
@@ -0,0 +1,16 @@
+﻿using CertainDeathEngine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertainDeathEngine.DAL
+{
+    public interface IDAL
+    {
+        void SaveWorld(GameWorld world);
+
+        GameWorld LoadWorld(int worldId);
+    }
+}
