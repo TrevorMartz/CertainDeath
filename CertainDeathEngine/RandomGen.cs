c2b2ebdb8f06480a6c584a1649c612ec328ba0eb
Trevor Martz <trevormartz1995@gmail.com>
2015-02-18 15:08:30 -0700

    RandomGen

---
+++ CertainDeathEngine/RandomGen.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace CertainDeathEngine
+{
+    public class RandomGen
+    {
+        public static Random Random { get; set; }
+
+        public static void Init()
+        {
+            string s = ConfigurationManager.AppSettings["RandomNumberSeed"];
+            try
+            {
+                Random = new Random(Convert.ToInt32(s));
+                Trace.WriteLine("seed " + s);
+            }
+            catch (Exception)
+            {
+                Trace.WriteLine("Error with the random number seed. Creating a default random object.");
+                Random = new Random();
+            }
+        }
+    }
+}
