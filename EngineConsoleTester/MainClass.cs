55db9a515feef39358cc7a28fd5c18047928882f
shunsaker <shunsaker@student.neumont.edu>
2015-02-19 09:30:20 -0700

    Added a console project for testing game engine

---
+++ EngineConsoleTester/MainClass.cs
@@ -0,0 +1,21 @@
+﻿using CertainDeathEngine.Models.World;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// This project will be used to provide console output for the CertainDeathEngine
+// for testing and debuging
+namespace EngineConsoleTester
+{
+	class MainClass
+	{
+		static void Main(string[] args)
+		{
+			GameWorldGenerator generator = new GameWorldGenerator();
+			Console.WriteLine(generator.GenerateTile().ToString());
+		}
+	}
+}
