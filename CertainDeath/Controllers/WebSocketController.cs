8c40a266bc2aa85dc5f01801e1757e821bb79105
runnerbos <runnerbos@users.noreply.github.com>
2015-02-20 14:57:32 -0700

    Client loads board and sends click events. Server sends and recieves.

---
+++ CertainDeath/Controllers/WebSocketController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Web.WebSockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using CertainDeathEngine.DAL;
+using CertainDeathEngine.Models.User;
+using System.Diagnostics;
+
+namespace CertainDeath.Controllers
+{
+    public class WebSocketController : ApiController
+    {
+        // GET: WebSocket
+        public HttpResponseMessage Get(int id)
+        {
+            HttpContext.Current.AcceptWebSocketRequest(new GameWebSocketHandler(id));
+            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        }
+
+        public class GameWebSocketHandler : WebSocketHandler
+        {
+            private int id;
+
+            public GameWebSocketHandler(int id)
+            {
+                this.id = id;
+            }
+            public override void OnMessage(string message)
+            {
+                Trace.WriteLine(message);
+            }
+
+            public override void OnOpen()
+            {
+                CertainDeathUser user = new CertainDeathUser() { Id = id };
+                FileDAL dal = new FileDAL();
+                Send(dal.LoadGame(user).ToJSON());
+            }
+
+            public override void OnClose()
+            {
+                Trace.WriteLine("[Connection closed]");
+            }
+        }
+    }
+}
