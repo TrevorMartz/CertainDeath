418d4cef0464bd7790bbabc63691800fadbfdf25
Blake Rollins <brollins90@gmail.com>
2015-02-23 16:16:21 -0700

    added an empty building factory

---
+++ CertainDeathEngine/Factories/BuildingFactory.cs
@@ -0,0 +1,24 @@
+﻿using CertainDeathEngine.Models.NPC;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CertainDeathEngine.Factories
+{
+    public class BuildingFactory
+    {
+        static int nextBuildingId = 1;
+
+        public BuildingFactory()
+        {
+
+        }
+
+        public Building BuildBuilding(string buildingType)
+        {
+            return new Building();
+        }
+    }
+}
