c1171314e02a3228d71815e76d42743942df08c4
Trevor Martz <trevormartz1995@gmail.com>
2015-02-25 15:40:23 -0700

    moved turrets to building folder

--- CertainDeathEngine/Models/NPC/Buildings/Turret.cs
+++ CertainDeathEngine/Models/NPC/Buildings/Turret.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace CertainDeathEngine.Models.NPC
+namespace CertainDeathEngine.Models.NPC.Buildings
 {
     [Serializable]
     [JsonObject(MemberSerialization.OptIn)]
