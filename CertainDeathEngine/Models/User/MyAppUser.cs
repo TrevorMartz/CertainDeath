04d18991c3c21beba2b392f72487e824eaa6773b
ellingit <joshuatheellington@gmail.com>
2015-02-26 16:08:50 -0700

    WOOO Kevin's MOM

--- CertainDeathEngine/Models/User/MyAppUser.cs
+++ CertainDeathEngine/Models/User/MyAppUser.cs
@@ -11,15 +11,15 @@ namespace CertainDeathEngine.Models.User
         public string Name { get; set; }
         public string Email { get; set; }
 
-        
-        //[JsonProperty("picture")] // This renames the property to picture.
-        //[FacebookFieldModifier("type(large)")] // This sets the picture size to large.
-        //public FacebookConnection<FacebookPicture> ProfilePicture { get; set; }
 
-        //[FacebookFieldModifier("limit(8)")] // This sets the size of the friend list to 8, remove it to get all friends.
-        //public FacebookGroupConnection<MyAppUserFriend> Friends { get; set; }
+        [JsonProperty("picture")] // This renames the property to picture.
+        [FacebookFieldModifier("type(large)")] // This sets the picture size to large.
+        public FacebookConnection<FacebookPicture> ProfilePicture { get; set; }
 
-        //[FacebookFieldModifier("limit(16)")] // This sets the size of the photo list to 16, remove it to get all photos.
-        //public FacebookGroupConnection<FacebookPhoto> Photos { get; set; }
+        [FacebookFieldModifier("limit(8)")] // This sets the size of the friend list to 8, remove it to get all friends.
+        public FacebookGroupConnection<MyAppUserFriend> Friends { get; set; }
+
+        [FacebookFieldModifier("limit(16)")] // This sets the size of the photo list to 16, remove it to get all photos.
+        public FacebookGroupConnection<FacebookPhoto> Photos { get; set; }
     }
 }
