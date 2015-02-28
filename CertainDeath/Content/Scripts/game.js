6a0356061a0dfa0cdef9ba98d9df27f9ebd4fb35
runnerbos <runnerbos@users.noreply.github.com>
2015-02-28 09:34:52 -0700

    Js screens now more modular. Network input delegated.

--- CertainDeath/Content/Scripts/game.js
+++ CertainDeath/Content/Scripts/game.js
@@ -27,17 +27,20 @@ function create () {
     // add all objects to game to be displayed
     //grass = new game.add.sprite(game.world.centerX, 500, 'grass', 'objects');
     //grass = game.add.tileSprite(0, 0, game.world.width, game.world.height, "objects", "grass");
-    View.current = new View.MainGameScreen(game, Server, 10, 10, game.width - 20, game.height - 20);
+    //View.current = new View.MainGameScreen(game, Server, 300, 30, game.width - 60, game.height - 60);
     var views = [
-        {
-            screen: new View.LoadingScreen(10, 10, game.width - 20, game.height - 20),
-            x: 10,
-            y: 10,
-        }
+        new View.InventoryBar(game, 10, 10, game.width - 20, 32*3),
+        new View.MainGameScreen(game, 30, 20 + 32 * 3, game.width - 60, game.height - 30 - 20 - 32 * 3),
     ];
     View.current = new View.ScreenContainer(views, 0, 0, game.width, game.height);
     View.current.create();
-    game.stage.backgroundColor = 0xbbddff;
+    Server.register(View.current);
+    View.current.screens.forEach(function (val) {
+        Server.register(val);
+    });
+    Server.open();
+    // Uncomment later
+    //game.stage.backgroundColor = 0xbbddff;
 }
 
 function update () {
@@ -48,14 +51,47 @@ function update () {
 function render () {
     // I don't know what this is for
     // Docs say it's not used often, so...
+    View.current.render();
 }
 
-//TODO: the 10 below is the game id that we need to get from the matching user session.
-var Server = new WebSocket("wss://" + window.location.host + "/api/WebSocket/10");
-Server.messages = new Array();
-Server.onmessage = function (message) {
-    this.messages.push(JSON.parse(message.data));
-}
-Server.next = function () {
-    return this.messages.pop();
-}
+Server = (function () {
+    var socket = null;
+    var listeners = new Array();
+    function open() {
+        socket = new WebSocket("wss://" + window.location.host + "/api/WebSocket/10");
+        socket.onmessage = onmessage;
+    }
+
+    function register(listener) {
+        listeners.push(listener);
+    }
+
+    function unregister(listener){
+        throw new Error("Not yet implemented.");
+    }
+
+    function onmessage(message){
+        //this.messages.push(JSON.parse(message.data));
+        console.log(message.data);
+        var obj = JSON.parse(message.data);
+        for (var x = 0; x < listeners.length; ++x) {
+            if (obj[listeners[x].subscribesTo]) {
+                listeners[x].onmessage(obj[listeners[x].subscribesTo]);
+            }
+        }
+    }
+
+    window.onbeforeunload = function () {
+        socket.close();
+    }
+
+    function close() {
+
+    }
+
+    return {
+        open: open,
+        register: register,
+        close: close
+    }
+})();
--- CertainDeath/Content/Scripts/view.js
+++ CertainDeath/Content/Scripts/view.js
@@ -4,20 +4,22 @@ View = (function () {
     /**
      * The base screen class
      **/
-    function Screen(x, y, width, height) {
+    function Screen(x, y, screenWidth, screenHeight) {
         // The coordinates of the screen
         if (x === undefined)
             x = 0;
         if (y === undefined)
             y = 0;
-        if (width === undefined)
+        if (screenWidth === undefined)
             throw new Error("width must be defined.");
-        if (height === undefined)
+        if (screenHeight === undefined)
             throw new Error("height must be defined.");
         this.x = x;
         this.y = y;
-        this.width = width;
-        this.height = height;
+        this.width = screenWidth;
+        this.height = screenHeight;
+        this.subscribesTo = null;
+        this.messages = Array();
     }
     Screen.prototype = {
         /**
@@ -25,11 +27,40 @@ View = (function () {
          */
         create: function () { },
 
-        update: function () { },
+        update: function () {
+            
+        },
 
         click: function (evt) { },
 
-        destroy: function () { }
+        destroy: function () {
+            this.g.destroy();
+        },
+
+        onmessage: function (obj){
+            messages.push(obj);
+        },
+
+        render: function () {
+            if (this.g === undefined && this.game !== undefined) {
+                var offset = 20;
+                this.g = this.game.add.graphics(0, 0);
+
+                this.g.lineStyle(2, 0x00FF00, 1);
+
+                this.g.moveTo(this.x - offset, this.y);
+                this.g.lineTo(this.x + this.width + offset, this.y);
+
+                this.g.moveTo(this.x - offset, this.y + this.height);
+                this.g.lineTo(this.x + this.width + offset, this.y + this.height);
+
+                this.g.moveTo(this.x, this.y - offset);
+                this.g.lineTo(this.x, this.y + this.height + offset);
+
+                this.g.moveTo(this.x + this.width, this.y - offset);
+                this.g.lineTo(this.x + this.width, this.y + this.height + offset);
+            }
+        }
     }
     Screen.prototype.constructor = Screen;
 
@@ -37,9 +68,11 @@ View = (function () {
      * The loading screen. Requires access to the game object.
      */
     function LoadingScreen(game, x, y, width, height) {
-        width = width || game.width;
-        height = height || game.height;
-        Screen.call(x, y, width, height);
+        if(width === undefined)
+            width = game.width;
+        if(height == undefined)
+            height = game.height;
+        Screen.call(this, x, y, width, height);
         this.text = null;
         this.graphics = null;
         this.game = game;
@@ -68,10 +101,12 @@ View = (function () {
      * @param game - A Phaser.js game object
      * @param server - A Server object to send and recieve 
      */
-    function MainGameScreen(game, server, x, y, width, height) {
-        width = width || game.width;
-        height = height || game.height;
-        Screen.call(x, y, width, height);
+    function MainGameScreen(game, x, y, screenWidth, screenHeight) {
+        if (screenWidth === undefined)
+            screenWidth = game.width;
+        if (screenHeight == undefined)
+            screenHeight = game.height;
+        Screen.call(this, x, y, screenWidth, screenHeight);
         this.game = game;
         this.server = server;
         this.objects = new Array();
@@ -80,7 +115,9 @@ View = (function () {
         this.squaresHigh = 0;
         this.boardX = 0;
         this.boardY = 0;
+        this.subscribesTo = "Squares";
     }
+
     MainGameScreen.prototype = Object.create(Screen.prototype, {
         create: {
             value: function(){
@@ -89,39 +126,15 @@ View = (function () {
         },
         update: {
             value: function () {
-                var msg = undefined;
-                while ((msg = this.server.next()) != undefined) {
-                    // Updating background...
-                    if (msg.Squares != 'undefined') {
-                        this.squaresWide = msg.Squares.length;
-                        this.squaresHigh = msg.Squares[0].length;
-
-                        this.boardX = (this.width - this.squaresWide * 32) / 2 + this.x;
-                        this.boardY = (this.height - this.squaresHigh * 32) / 2 + this.y;
-
-                        for (var i = 0; i < msg.Squares.length; i++) {
-                            for (var j = 0; j < msg.Squares[i].length; j++) {
-                                objects = new Array();
-                                var rand = Math.ceil(Math.random() * 3);
-                                objects += game.add.sprite(i * 32 + this.boardX, j * 32 + this.boardY,
-                                    "objects", msg.Squares[i][j].TypeName + rand);
-                                if (msg.Squares[i][j].ResourceName !== undefined) {
-                                    console.log(msg.Squares[i][j].ResourceName);
-                                    objects += game.add.sprite(i * 32 + this.boardX, j * 32 + this.boardY,
-                                        "objects", msg.Squares[i][j].ResourceName);
-                                }
-                            }
-                        }
-                    }
-                }
+                Screen.prototype.update.call(this);
 
                 // Now check the user's input
                 if (game.input.activePointer.isDown && !this._pointerDown) {
                     this._pointerDown = true;
                     Server.send(JSON.stringify({
                         "event": "click",
-                        "x": game.input.activePointer.x / (game.world.width / this.squaresWide),
-                        "y": game.input.activePointer.y / (game.world.height / this.squaresHigh)
+                        "x": (game.input.activePointer.x - this.x) / (this.width / this.squaresWide),
+                        "y": (game.input.activePointer.y - this.y) / (this.height / this.squaresHigh)
                     }));
                 }
 
@@ -136,6 +149,43 @@ View = (function () {
                     obj.destroy();
                 }
             }
+        },
+        onmessage: {
+            value: function (msg) {
+                if (msg != 'undefined') {
+                    this.squaresWide = msg.length;
+                    this.squaresHigh = msg[0].length;
+
+                    var tileSize = Math.min(this.height / this.squaresHigh, this.width / this.squaresWide);
+
+                    // Make the board snap to a good scale so that the images still look crisp
+                    if (tileSize > 64)
+                        tileSize = 64;
+                    else if (tileSize > 32)
+                        tileSize = 32;
+
+                    this.boardX = (this.width - this.squaresWide * tileSize) / 2 + this.x;
+                    this.boardY = (this.height - this.squaresHigh * tileSize) / 2 + this.y;
+
+                    for (var i = 0; i < msg.length; i++) {
+                        for (var j = 0; j < msg[i].length; j++) {
+                            objects = new Array();
+                            var rand = Math.ceil(Math.random() * 3);
+                            var sprite = game.add.sprite(i * tileSize + this.boardX, j * tileSize + this.boardY,
+                                "objects", msg[i][j].TypeName + rand);
+                            sprite.scale.setTo(tileSize / sprite.width);
+                            objects.push(sprite);
+
+                            if (msg[i][j].ResourceName !== undefined) {
+                                var sprite2 = game.add.sprite(i * tileSize + this.boardX, j * tileSize + this.boardY,
+                                    "objects", msg[i][j].ResourceName);
+                                sprite2.scale.setTo(tileSize / sprite2.width);
+                                objects.push(sprite2);
+                            }
+                        }
+                    }
+                }
+            }
         }
     });
 
@@ -145,11 +195,11 @@ View = (function () {
      * in the order that you want their lifecycle methods to be called.
      */
     function ScreenContainer(screens, x, y, width, height) {
-        Screen.call(x, y, width, height);
+        Screen.call(this, x, y, width, height);
 
-        if (!(typeof (screens) === typeof (Array) && screen in screens[0] && x in screens[0] && y in screens[0])) {
-            throw new Error("The object passed into GameScreen did not match the expected format.")
-        }
+        //if (!(screens === undefined && screen in screens[0] && x in screens[0] && y in screens[0])) {
+        //    throw new Error("The object passed into GameScreen did not match the expected format.")
+        //}
 
         this.screens = screens;
     }
@@ -157,30 +207,72 @@ View = (function () {
     ScreenContainer.prototype = Object.create(Screen.prototype, {
         create: {
             value: function () {
-                for (var x in screens) {
-                    x.screen.create();
+                for (var x = 0; x < this.screens.length; ++x) {
+                    this.screens[x].create();
                 }
             }
         },
         update: {
             value: function () {
-                for (var x in screens) {
-                    x.screen.update();
+                for (var x = 0; x < this.screens.length; ++x) {
+                    this.screens[x].update();
                 }
             }
         },
         click: {
             value: function (evt) {
-                for (var x in screens) {
-                    x.screen.click();
+                for (var x = 0; x < this.screens.length; ++x) {
+                    this.screens[x].click();
                 }
             }
         },
         destroy: {
             value: function () {
-                for (var x in screens) {
-                    x.screen.destroy();
+                for (var x = 0; x < this.screens.length; ++x) {
+                    this.screens[x].destroy();
                 }
+            }
+        },
+        render: {
+            value: function () {
+                for (var x = 0; x < this.screens.length; ++x) {
+                    this.screens[x].render();
+                }
+            }
+        }
+    });
+
+    // Height is set
+    function InventoryBar(game, x, y, width, height) {
+        Screen.call(this, x, y, width, height);
+        this.values = Array();
+        this.game = game;
+    }
+
+    InventoryBar.prototype = Object.create(Screen.prototype, {
+        //create: {
+        //    value: function () {
+        //        // Not much we can do here...
+        //    }
+        //},
+        update: {
+            value: function () {
+                Screen.prototype.update.call(this);
+            }
+        },
+        //click: {
+        //    value: function (evt) {
+                
+        //    }
+        //},
+        destroy: {
+            value: function () {
+                
+            }
+        },
+        onmessage: {
+            value: function (msg) {
+                this.values = msg;
             }
         }
     });
@@ -189,7 +281,8 @@ View = (function () {
         MainGameScreen: MainGameScreen,
         LoadingScreen: LoadingScreen,
         ScreenContainer: ScreenContainer,
+        InventoryBar : InventoryBar,
         Screen: Screen,
-        current: new Screen()
+        current: new Screen(0, 0, 100, 100)
     }
 })();
