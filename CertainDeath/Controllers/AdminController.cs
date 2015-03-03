59e5b544d5759e3f6afa03e89fa2df846d592688
Blake Rollins <brollins90@gmail.com>
2015-03-03 08:00:03 -0700

    Fixed a resource collecting bug

--- CertainDeath/Controllers/AdminController.cs
+++ CertainDeath/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CertainDeathEngine.DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,26 @@ namespace CertainDeath.Controllers
 {
     public class AdminController : Controller
     {
+        private readonly IGameDAL _gameDal;
+        private readonly IUserDAL _userDal;
+        private readonly IStatisticsDAL _statisticsDal;
+
+        public AdminController(IGameDAL gameDal, IUserDAL userDal, IStatisticsDAL statisticsDal)
+        {
+            this._gameDal = gameDal;
+            this._userDal = userDal;
+            this._statisticsDal = statisticsDal;
+        }
+
         // GET: Admin
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult Users()
+        {
+            return View("Users", _userDal.GetAllUsers());
         }
     }
 }
