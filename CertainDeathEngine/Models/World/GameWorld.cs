6d9256a0f22df7d199402e66681fab44a8ac778a
ellingit <joshuatheellington@gmail.com>
2015-03-10 16:31:35 -0600

    ...

--- CertainDeathEngine/Models/World/GameWorld.cs
+++ CertainDeathEngine/Models/World/GameWorld.cs
@@ -22,27 +22,13 @@ namespace CertainDeathEngine.Models
         public static Dictionary<BuildingType, Cost>
             BuildingCostsForTheWorld = new Dictionary<BuildingType, Cost>()
             {
-                {
-                    BuildingType.FIRE_OF_LIFE, new FireOfLife(null).Cost
-                },
-                {
-                    BuildingType.AUTO_HARVESTER_MINE, new Cost() { Costs = { { ResourceType.COAL, 10 }, { ResourceType.STONE, 25 } } }
-                },
-                {
-                    BuildingType.AUTO_HARVESTER_QUARRY, new Cost() { Costs = { { ResourceType.IRON, 25 }, { ResourceType.COAL, 20 } } }
-                },
-                {
-                    BuildingType.AUTO_HARVESTER_FARM, new Cost() { Costs = { { ResourceType.WOOD, 25 }, { ResourceType.COAL, 15 } } }
-                },
-                {
-                    BuildingType.AUTO_HARVESTER_LUMBER_MILL, new Cost() { Costs = { { ResourceType.STONE, 20 }, { ResourceType.IRON, 15 } } }
-                },
-                {
-                    BuildingType.TURRET, new Cost { }
-                },
-                {
-                    BuildingType.WALL, new Cost { }
-                }
+                { BuildingType.FIRE_OF_LIFE, new FireOfLife(null).Cost },
+                { BuildingType.AUTO_HARVESTER_MINE, new Cost() { Costs = { { ResourceType.COAL, 10 }, { ResourceType.STONE, 25 } } } },
+                { BuildingType.AUTO_HARVESTER_QUARRY, new Cost() { Costs = { { ResourceType.IRON, 25 }, { ResourceType.COAL, 20 } } } },
+                { BuildingType.AUTO_HARVESTER_FARM, new Cost() { Costs = { { ResourceType.WOOD, 25 }, { ResourceType.COAL, 15 } } } },
+                { BuildingType.AUTO_HARVESTER_LUMBER_MILL, new Cost() { Costs = { { ResourceType.STONE, 20 }, { ResourceType.IRON, 15 } } } },
+                { BuildingType.TURRET, new Cost { } },
+                { BuildingType.WALL, new Cost { } }
             };
 
         [NonSerialized]
